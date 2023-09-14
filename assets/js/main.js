733ad7678f1ff6c2129c6b61b88c443f40fabde3
Daniel <thedezz360@gmail.com>
2023-09-14 12:57:04 +0200

    services

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,70 @@ function linkAction (){
 }
 
 navLink.forEach(link => link.addEventListener('click', linkAction))
+
+/** ACCORDION SKILLS */
+const skillsContent = document.getElementsByClassName('skills_content')
+const skillsHeader = document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+  let itemClass = this.parentNode.className
+  console.log(itemClass)
+  for(i= 0 ; i < skillsContent.length ; i++){
+    skillsContent[i].className = 'skills_content skills_close'
+  }
+  if(itemClass === 'skills_content skills_close'){
+    this.parentNode.className = 'skills_content skills_open'
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills)
+})
+
+/** QUALIFICATION TABS */
+const tabs = document.querySelectorAll('[data-target]')
+console.log(tabs)
+const tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', ()=>{
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification_active')
+    })
+    target.classList.add('qualification_active')
+
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification_active')
+    })
+
+    tab.classList.add('qualification_active')
+  })
+})
+
+
+/** SERVICES MODAL */
+const modalViews = document.querySelectorAll('.services_modal')
+const modalBtns = document.querySelectorAll('.services_button')
+const modalCloses = document.querySelectorAll('.services_modal-close')
+
+
+
+const modal = (modalClick) => {
+  modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener('click', ()=>{
+    modal(i)
+  })
+})
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener('click', () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove('active-modal')
+    })
+  })
+})
