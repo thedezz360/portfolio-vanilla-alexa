7c9aea132540e3433f5b8c6f432e0577b5d3a637
Daniel <thedezz360@gmail.com>
2023-09-15 14:09:20 +0200

    active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 /* MENU SHOW Y HIDDEN*/
 const navMenu = document.getElementById('nav-menu')
-const navToggle= document.getElementById('nav-toggle')
+const navToggle = document.getElementById('nav-toggle')
 const navClose = document.getElementById('nav-close')
 
 /** Menu show */
 /** Validate if constant exist */
-if(navToggle){
-  navToggle.addEventListener('click', ()=> {
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
     navMenu.classList.add('show-menu')
   })
 }
 
 /** Menu hidden */
 /** Validate if constant exist */
-if(navClose){
+if (navClose) {
   navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu')
   })
@@ -22,25 +22,25 @@ if(navClose){
 //** REMOVE MENU MOBILE */
 const navLink = document.querySelectorAll('.nav_link')
 
-function linkAction (){
+function linkAction() {
   const navMenu = document.getElementById('nav-menu')
   // when we click on each nav_link, we remove the show-menu class
   navMenu.classList.remove('show-menu')
 }
 
-navLink.forEach(link => link.addEventListener('click', linkAction))
+navLink.forEach((link) => link.addEventListener('click', linkAction))
 
 /** ACCORDION SKILLS */
 const skillsContent = document.getElementsByClassName('skills_content')
 const skillsHeader = document.querySelectorAll('.skills_header')
 
-function toggleSkills(){
+function toggleSkills() {
   let itemClass = this.parentNode.className
   console.log(itemClass)
-  for(i= 0 ; i < skillsContent.length ; i++){
+  for (i = 0; i < skillsContent.length; i++) {
     skillsContent[i].className = 'skills_content skills_close'
   }
-  if(itemClass === 'skills_content skills_close'){
+  if (itemClass === 'skills_content skills_close') {
     this.parentNode.className = 'skills_content skills_open'
   }
 }
@@ -54,16 +54,16 @@ const tabs = document.querySelectorAll('[data-target]')
 console.log(tabs)
 const tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', ()=>{
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.target)
 
-    tabContents.forEach(tabContent => {
+    tabContents.forEach((tabContent) => {
       tabContent.classList.remove('qualification_active')
     })
     target.classList.add('qualification_active')
 
-    tabs.forEach(tab => {
+    tabs.forEach((tab) => {
       tab.classList.remove('qualification_active')
     })
 
@@ -71,20 +71,17 @@ tabs.forEach(tab => {
   })
 })
 
-
 /** SERVICES MODAL */
 const modalViews = document.querySelectorAll('.services_modal')
 const modalBtns = document.querySelectorAll('.services_button')
 const modalCloses = document.querySelectorAll('.services_modal-close')
-
-
 
 const modal = (modalClick) => {
   modalViews[modalClick].classList.add('active-modal')
 }
 
 modalBtns.forEach((modalBtn, i) => {
-  modalBtn.addEventListener('click', ()=>{
+  modalBtn.addEventListener('click', () => {
     modal(i)
   })
 })
@@ -96,3 +93,60 @@ modalCloses.forEach((modalClose) => {
     })
   })
 })
+
+/** PORTFOLIO SWIPER */
+var swiperPortfolio = new Swiper('.swiperPortfolio', {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+})
+
+var swiperTestimonial = new Swiper('.swiperTestimonial', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+    clickable: true
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2
+    }
+  }
+})
+
+/** SCROLL SECTION ACTIVE LINK */
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.scrollY
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight
+
+    const sectionTop = current.offsetTop - 50
+
+    const sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(`.nav_menu a[href*=${sectionId}]`)
+        .classList.add('active-link')
+    } else {
+      document
+        .querySelector(`.nav_menu a[href*=${sectionId}]`)
+        .classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
